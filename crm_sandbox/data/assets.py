23b4506726bf72ae9fa85f66b7905678b1da9ab4
Steeve Huang <kh.huang@salesforce.com>
2024-11-06 15:33:28 -0800

    add workaround for huggingface's data representations

--- crm_sandbox/data/assets.py
+++ crm_sandbox/data/assets.py
@@ -7,5 +7,9 @@ TASKS_NATURAL = [data for data in crmarena["test"]]
 schema = load_dataset("Salesforce/CRMArena", "schema")
 
 SCHEMA = [data for data in schema["test"]]    
+
+# work around for huggingface's data representations
+for schema in SCHEMA:
+    schema["fields"] = {k: v for k, v in schema["fields"].items() if v is not None}
     
 TOOLS = None
